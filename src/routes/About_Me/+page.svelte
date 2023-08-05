af41d1d62b5329502e3f5d2426bf03644ec91608
Riwaj Mainali <riwaj227@gmail.com>
2023-08-05 04:07:23 -0500

    actual initial commit :)

---
+++ src/routes/About_Me/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+  <title>About Me</title>
+  <meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+  <p>
+    lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl
+    eu
+  </p>
+</div>
+
+<style>
+  p {
+    font-size: 1.1em;
+  }
+</style>
---
+++ src/routes/Blog/+page.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+  <title>Blog</title>
+  <meta name="description" content="Blog" />
+</svelte:head>
+<div class="text-column">
+  <h1>Blog</h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl
+    eu
+  </p>
+</div>
